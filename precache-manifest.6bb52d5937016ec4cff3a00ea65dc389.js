fbf437f567252f078a103d66c3fdc493d625902f
lcavuoti <lcavuoti@clip.ch>
2022-04-03 17:54:56 +0200

    Updates

---
+++ precache-manifest.6bb52d5937016ec4cff3a00ea65dc389.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f80a5eb080e2e262a3d5",
+    "url": "/03-fetching-func-snapshot-gh-pages/static/css/main.9954b890.chunk.css"
+  },
+  {
+    "revision": "f80a5eb080e2e262a3d5",
+    "url": "/03-fetching-func-snapshot-gh-pages/static/js/main.f80a5eb0.chunk.js"
+  },
+  {
+    "revision": "2fedc729a9ae31ee1695",
+    "url": "/03-fetching-func-snapshot-gh-pages/static/js/1.2fedc729.chunk.js"
+  },
+  {
+    "revision": "bdef62a7e86bf044a99c",
+    "url": "/03-fetching-func-snapshot-gh-pages/static/js/runtime~main.bdef62a7.js"
+  },
+  {
+    "revision": "2cd9efe715fde9e92f2309c87034c7d0",
+    "url": "/03-fetching-func-snapshot-gh-pages/index.html"
+  }
+];
